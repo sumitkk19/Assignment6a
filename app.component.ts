d0d28423c2b8df23d906f142b744c6febdb2aff8
sumitkk19 <65333951+sumitkk19@users.noreply.github.com>
2023-04-15 23:41:05 +0530

    Add files via upload

---
+++ app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl:
+    <style>
+    <h1 style="color:blue;"> Marvellous Infosystems</h1>
+
+  <input type Text >;
+  </style>
+  
+    //'./app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Assignment6a';
+}
+
